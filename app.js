eb4b2be07cde74508289ded856fd8dafee6976fe
Caique Bispo <caiquebispo0023@gmail.com>
2022-03-07 06:54:44 -0300

    Inciando o Projeto, criando arquitetura e esquema de rotas

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const firebird = require('node-firebird')
+const app = express()
+const port = 3000
+ 
+//starting database connection variables
+var options = {};
+options.database = 'C:/TESTE/DATABASE.FDB';
+options.user = 'SYSDBA';
+options.password = 'masterkey';
+
+app.get('/',(request,response) =>{
+
+    firebird.attach(options, function(err, db) {
+        db.query('SELECT * FROM PERSON', function(err, result) {
+            
+            for (let i = 0; i < result.length; i++) {                        
+                for (const [key, value] of Object.entries(result[i])) {
+                    if(Buffer.isBuffer(value)){
+                    result[i][key] =  `${value}`; 
+                    }
+                }
+            }
+            return response.json(result)
+            db.detach();
+        });
+    })
+})
+app.listen(port, () => {
+    console.log(`O Servidor está rodando na porta ${port}`)
+})
