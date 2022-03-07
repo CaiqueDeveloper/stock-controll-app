b59375cf0d936aa407813b0af03247a39f2277bb
Caique Bispo <caiquebispo0023@gmail.com>
2022-03-07 17:59:44 -0300

    Teste Run Heroku

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const firebird = require('node-firebird')
+fs = require('fs')
 const app = express()
 const port = process.env.PORT || 3000
  
@@ -10,25 +11,22 @@ options.user = 'SYSDBA';
 options.password = 'masterkey';
 
 app.get('/',(request,response) =>{
-    
-    firebird.attach(options, function(err, db) {
-         //response.send(db)
-        db.query('SELECT * FROM PERSON', function(err, result) {
-          response.send("Hello dentro da function 1")
+    response.send("Hello")
+    // firebird.attach(options, function(err, db) {
+    //     db.query('SELECT * FROM PERSON', function(err, result) {   
            
-            
-            // for (let i = 0; i < result.length; i++) {                        
-            //     for (const [key, value] of Object.entries(result[i])) {
-            //         if(Buffer.isBuffer(value)){
-            //         result[i][key] =  `${value}`; 
-            //         }
-            //     }
-            // }
-            // console.log(result)
-            // return response.json(result)
-            // db.detach();
-        });
-    })
+    //         for (let i = 0; i < result.length; i++) {                        
+    //             for (const [key, value] of Object.entries(result[i])) {
+    //                 if(Buffer.isBuffer(value)){
+    //                 result[i][key] =  `${value}`; 
+    //                 }
+    //             }
+    //         }
+    //         console.log(result)
+    //         return response.json(result)
+    //         db.detach();
+    //     });
+    // })
 })
 app.listen(port, () => {
     console.log(`O Servidor está rodando na porta ${port}`)
