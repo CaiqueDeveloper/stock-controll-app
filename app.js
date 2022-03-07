d65a829475584fbb958231cd72210db335fee763
Caique Bispo <caiquebispo0023@gmail.com>
2022-03-07 11:14:18 -0300

    Configurado Heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const firebird = require('node-firebird')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
  
 //starting database connection variables
 var options = {};
